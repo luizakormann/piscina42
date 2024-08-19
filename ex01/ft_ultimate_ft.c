62ad96226408cd561cb52b205af4492477d2ecc8
Luiza Kormann Carvalho Da Fonseca <lukorman@student.42sp.org.br>
2024-08-19 20:06:15 -0300

    compile check ok norminette ok

---
+++ ex01/ft_ultimate_ft.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lukorman <lukorman@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/19 18:03:06 by lukorman          #+#    #+#             */
+/*   Updated: 2024/08/19 20:05:07 by lukorman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include <stdio.h>
+
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+
+// int main(void)
+// {
+//     int *point1;
+//     int **point2;
+//     int ***point3;
+//     int ****point4;
+//     int *****point5;
+//     int ******point6;
+//     int *******point7;
+//     int ********point8;
+//     int *********point9;
+//     int value = 42;
+
+//     point1 = &value;
+//     point2 = &point1;
+//     point3 = &point2;
+//     point4 = &point3; 
+//     point5 = &point4;
+//     point6 = &point5;
+//     point7 = &point6;
+//     point8 = &point7;
+//     point9 = &point8;
+
+//     ft_ultimate_ft(point9);
+//     printf("O endereço de memória: %p.\n", &point9);
+//     printf("O valor: %d.", *********point9);
+//     return(0);
+// }
