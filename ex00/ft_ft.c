c6415c0d799f75f10079a418c54b91297f355d69
Luiza Kormann Carvalho Da Fonseca <lukorman@student.42sp.org.br>
2024-08-19 17:54:14 -0300

    compile check ok norminette ok

--- ex00/ft_ft.c
+++ ex00/ft_ft.c
@@ -3,11 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   ft_ft.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lukorman <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: lukorman <lukorman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/16 18:32:08 by lukorman          #+#    #+#             */
-/*   Updated: 2024/08/16 20:07:36 by lukorman         ###   ########.fr       */
+/*   Updated: 2024/08/19 17:48:52 by lukorman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 
+void	ft_ft(int *nbr)
+{
+	*nbr = 42;
+}
+
+// int	main(void)
+// {
+// 	int	value;
+// 	ft_ft(&value);
+// 	printf("O endereço de memória é: %p.\n", &value);
+// 	printf("O valor é: %d.", value);
+// }
