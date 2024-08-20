619d4ba6ce7d9d678307b5bcce486c6ed39e5715
Luiza Kormann Carvalho Da Fonseca <lukorman@student.42sp.org.br>
2024-08-20 02:40:27 -0300

    include and main commented

--- ex03/ft_div_mod.c
+++ ex03/ft_div_mod.c
@@ -6,11 +6,11 @@
 /*   By: lukorman <lukorman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 00:38:55 by lukorman          #+#    #+#             */
-/*   Updated: 2024/08/20 02:37:02 by lukorman         ###   ########.fr       */
+/*   Updated: 2024/08/20 02:39:27 by lukorman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+//#include <stdio.h>
 
 void	ft_div_mod(int a, int b, int *div, int *mod)
 {
@@ -23,7 +23,7 @@ void	ft_div_mod(int a, int b, int *div, int *mod)
 	*mod = modr;
 }
 
-int	main(void)
+/*int	main(void)
 {
 	int	divr;
 	int	modr;
@@ -31,4 +31,4 @@ int	main(void)
 	ft_div_mod(5, 2, &divr, &modr);
 	printf("Resultado divisão: %d.\nResto: %d.", divr, modr);
 	return (0);
-}
+}*/
